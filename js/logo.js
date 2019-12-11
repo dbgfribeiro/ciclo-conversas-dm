d890a025968489e72dca87b5d7e4eb2e89b7c28e
dbgfribeiro <dribeiro641@gmail.com>
2019-12-11 15:11:06 +0000

    Menu done, animation on scroll almost completed

--- js/logo.js
+++ js/logo.js
@@ -1,29 +1,50 @@
+/*
+var w = window.innerWidth
+|| document.documentElement.clientWidth
+|| document.body.clientWidth;
+
+var h = window.innerHeight
+|| document.documentElement.clientHeight
+|| document.body.clientHeight;
+console.log('width:' + w + ' height:' + h);
+*/
+
+var barra = document.getElementById('barra');
+var globo = document.getElementById('globo');
+var retina = document.getElementById('retina');
+var iris = document.getElementById('iris');
 var rotated = false;
 
-function myFunction() {
-  var elmnt = document.getElementById("outer-wrapper");
+var elmnt = document.getElementById("outer-wrapper");
+
+
+function scrolling() {
   var scroll = elmnt.scrollTop/2;
-  console.log("Scrolling: " + scroll + "px");
+  //console.log("Scrolling: " + scroll + "px");
 
   /*-----BARRA-----*/
-  var barra = document.getElementById('barra');
-  barra.style.webkitTransform = 'rotate(' + scroll + 'deg)' + 'scaleY(' + scroll / 100 + ')' + 'scaleX(' + scroll / 100 + ')';
-  barra.style.transform = 'rotate(' + scroll + 'deg)' + 'scaleY(' + scroll / 100 + ')' + 'scaleX(' + scroll / 100 + ')';
+  var bWidth= barra.clientWidth;
+  barra.style.webkitTransform = 'rotate(' + scroll + 'deg)';
+  barra.style.transform = 'rotate(' + scroll + 'deg)';
 
   /*-----GLOBO-----*/
-  var globo = document.getElementById('globo');
-  globo.style.webkitTransform = 'rotate(' + scroll + 'deg)' + 'scaleY(' + scroll / 100 + ')' + 'scaleX(' + scroll / 100 + ')';
-  globo.style.transform = 'rotate(' + scroll + 'deg)' + 'scaleY(' + scroll / 100 + ')' + 'scaleX(' + scroll / 100 + ')';
+  var gWidth= globo.clientWidth;
+  globo.style.webkitTransform = 'rotate(' + scroll + 'deg)';
+  globo.style.transform = 'rotate(' + scroll + 'deg)';
+
+
 
   /*-----RETINA-----*/
-  var retina = document.getElementById('retina');
-  retina.style.webkitTransform = 'scaleY(' + scroll / 100 + ')' + 'scaleX(' + scroll / 100 + ')';
-  retina.style.transform = 'scaleY(' + scroll / 100 + ')' + 'scaleX(' + scroll / 100 + ')';
+  var rWidth= retina.clientWidth;
+  retina.style.width = (rWidth + (scroll/500)) + "px";
+
+
 
   /*-----IRIS-----*/
-  var iris = document.getElementById('iris');
-  iris.style.webkitTransform = 'scaleY(' + scroll / 100 + ')' + 'scaleX(' + scroll / 100 + ')';
-  iris.style.transform = 'scaleY(' + scroll / 100 + ')' + 'scaleX(' + scroll / 100 + ')';
+  var iWidth= iris.clientWidth;
+  iris.style.width = (iWidth + (scroll/300)) + "px";
+
+
 
   rotated = !rotated;
 }
